033c08777b6d02b0ca503500e028585f660d533b
KanXd <Kantaphonkit@gmail.com>
2025-04-23 23:56:53 +0800

    Update main control flow

--- OOP_Final_project/Main.cpp
+++ OOP_Final_project/Main.cpp
@@ -5,16 +5,34 @@
 using namespace std;
 
 int main() {
+
+	char option;
     
-    Database db("project.db");
+    while (1) {
 
-    db.createStudentTable();
-    db.insertStudent("Alice");
-    db.insertStudent("Bob");
-
-    db.showAllStudents();
-
-    return 0;
+        cout << "---------------- " << "Welcome to the Airport Management System" << " ----------------" << endl;
+		cout << "Choose your option below:" << endl;
+		cout << "1. Modify flight Database" << endl;
+		cout << "2. Booking system" << endl; 
+		cout << "3. Exit" << endl;
+		cout << "Enter your option: ";
+		cin >> option;
+		switch (option) {
+			case '1':
+			{
+				cout << "option 1 was called" << endl;
+				break;
+			}
+			case '2':
+			{
+				cout << "option 2 was called" << endl;
+				break;
+			}
+			case '3':
+				cout << "Exiting the program." << endl;
+				return 0;
+		}
+    }
 
     return 0;
 }
